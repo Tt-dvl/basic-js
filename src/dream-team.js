8492a866ef203154684db55dedf5902cc7ef6cbc
Tetiana Kalko <taniakalko@gmail.com>
2020-10-17 12:14:32 +0300

    feat: add new functions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let out =false;
+  if(!(Array.isArray(members)))return out;
+
+  members = members.filter((el) => {
+    let compaundFilter = (typeof (el) === 'string' ) && (!(Array.isArray(el))) && (el !== 'undefined');
+
+    return compaundFilter;
+  })
+  
+  const arrMap = members.map(el => el.trim().toUpperCase()).sort().map(el => {
+    return el[0]
+  }).join('');
+ 
+  out = arrMap;
+  return out;
 };
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  throw new CustomError('Not implemented');	  
+ 
+  let out = {
+    turns: 0,
+    seconds: 0
+  }
+  out['turns']  = (2**disksNumber) -1;
+  out['seconds'] = Math.floor((out['turns'] / turnsSpeed) * 3600 ) ;
+
+  return out
+};	
